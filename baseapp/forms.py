466c38e74d826e27f89f65dbc7fcf774569e845e
vscorza0093 <vinicius.scorza@hotmail.com>
2023-07-25 18:17:17 -0300

    Django ORM e Migrations

--- baseapp/forms.py
+++ baseapp/forms.py
@@ -4,11 +4,12 @@ from django import forms
 class ContactForm(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
+    date = forms.DateTimeField()
     message = forms.CharField(widget=forms.Textarea)
 
 
 class PetShowerForm(forms.Form):
     petName = forms.CharField()
     phone = forms.CharField()
-    bookingDate = forms.DateField()
+    bookingDate = forms.DateTimeField()
     observations = forms.CharField()
--- baseapp/models.py
+++ baseapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=75)
+    date = models.DateTimeField(auto_now_add=True)
+    message = models.TextField()
